2f9ca4ea144fb3c2f9312e08e7fd766efe8bf4d7
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-12 08:25:20 -0700

    Create db seed and add destroy dependency to models

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+User.destroy_all
+
+User.create!([{
+  email: "fraiser@email.com",
+  password: "gabbagoo"
+},
+{
+	email: "niles@email.com",
+	password: "gabbagoo"
+}])
