a83480bc68e149ef9e2e848990a92e830b26dc7a
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-12 16:15:11 -0700

    Add Follow model and set up associations with User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
 
   has_many :posts, :dependent => :destroy
   has_many :comments, :dependent => :destroy
+  has_many :likes, :dependent => :destroy
+
+  #follows relationships
+  has_many :follows
+
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
+
 end
