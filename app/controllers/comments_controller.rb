833cd1b452af26dd223419d297f9a8b1ad3bfb04
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-15 14:31:13 -0700

    Add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+
+	def new
+		@comment = Comment.new
+	end
+
+	def create
+	  @comment = Comment.new(comment_params)
+
+	  respond_to do |format|
+	    if @comment.save
+	      format.html { redirect_to @comment.post, notice: "Comment was successfully created." }
+	    else
+	      format.html { render :new, status: :unprocessable_entity }
+	    end
+	  end
+	end
+
+
+
+
+	private
+
+	def comment_params
+	 params.require(:comment).permit(:post_id, :text)
+	end
+
+end
