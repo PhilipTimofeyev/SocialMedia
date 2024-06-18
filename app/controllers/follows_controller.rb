15aa22db17dad4a761576c92ce0f85b7bd04e5a4
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-18 16:53:24 -0700

    Refactor follows controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,43 +1,43 @@
 class FollowsController < ApplicationController
 
-	def create
-		# debugger
-    requesting_user_id = params[:user_id].to_i
-    requesting_user = User.find_by_id(requesting_user_id)
+	before_action :set_requestor
 
+	def create
     follow = Follow.new 
     follow.follower = current_user
-    follow.following = requesting_user
+    follow.following = @requesting_user
     follow.accepted = false
 
-    if follow.save
-      FollowChannel.broadcast_to(requesting_user, { user_id: current_user, template: 'request' })
-    end
-
+    unless follow_exists?
+	    if follow.save
+	      FollowChannel.broadcast_to(@requesting_user, { user_id: current_user, template: 'request' })
+	    end
+	  end
 	end
 
 	def update
-		debugger
-	  requesting_user_id = params[:user_id].to_i
-	  requesting_user = User.find_by_id(requesting_user_id)
-
-	  follow = Follow.find_by(follower_id: requesting_user_id, following_id: current_user.id)
+	  follow = Follow.find_by(follower_id: @requesting_user.id, following_id: current_user.id)
 	  follow.accepted = true
 
 	  if follow.save
-	    FollowChannel.broadcast_to(requesting_user, { user_id: current_user, template: 'accept' })
+	    FollowChannel.broadcast_to(@requesting_user, { user_id: current_user, template: 'accept' })
 	  end
 	end
 
 	def destroy
-		# debugger
-	  requesting_user_id = params[:user_id].to_i
-	  requesting_user = User.find_by_id(requesting_user_id)
-
-	  follow = Follow.find_by(follower_id: current_user.id, following_id: requesting_user_id)
+	  follow = Follow.find_by(follower_id: current_user.id, following_id: @requesting_user.id)
 	  follow.destroy
-	  # render partial: "following", locals: { user: @current_user}
+	end
 
+	private
+
+	def follow_exists?
+		Follow.exists?(follower_id: current_user.id, following_id: @requesting_user.id)
+	end
+
+	def set_requestor
+		requesting_user_id = params[:user_id].to_i
+		@requesting_user = User.find_by_id(requesting_user_id)
 	end
 
 end
