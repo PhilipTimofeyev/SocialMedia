422f0e263c752fa1a877f0d102af97ea435fbe23
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-21 22:34:56 -0700

    Add validation and error messages for post and comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,15 +5,15 @@ class CommentsController < ApplicationController
 	end
 
 	def create
-		post = Post.find(params[:post_id])
-	  comment = post.comments.build(comment_params)
-	  comment.user_id=current_user.id
+		@post = Post.find(params[:post_id])
+	  @comment = @post.comments.build(comment_params)
+	  @comment.user_id=current_user.id
 
 	  respond_to do |format|
-	    if comment.save
-	      format.html { redirect_to comment.post, notice: "Comment was successfully created." }
+	    if @comment.save
+	      redirect_to @comment.post, notice: "Comment was successfully created."
 	    else
-	      format.html { render :new, status: :unprocessable_entity }
+	      format.html { render :new, status: :unprocessable_entity}
 	    end
 	  end
 	end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, :dependent => :destroy
   has_many :likes, as: :likeable, :dependent => :destroy
+
+  validates :title, presence: true, length: { in: 2..30 }
+  validates :content, presence: true, length: { minimum: 3 }
 end
