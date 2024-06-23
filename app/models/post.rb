0742e6dd1f67748b91b5d557b0a4fc926a7bfc14
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-23 09:37:30 -0700

    Add active storage and update post controller and views to allow images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+  
   has_many :comments, :dependent => :destroy
   has_many :likes, as: :likeable, :dependent => :destroy
+
+  has_one_attached :image
 
   validates :title, presence: true, length: { in: 2..30 }
   validates :content, presence: true, length: { minimum: 3 }
