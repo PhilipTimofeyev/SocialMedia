5aa1569aa5cdda04168bcd32a961c8f266fb3d73
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-23 10:15:53 -0700

    Add action text to post, mini_magick gem, and process image into thumbnail

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,12 @@ class Post < ApplicationRecord
   has_many :likes, as: :likeable, :dependent => :destroy
 
   has_one_attached :image
+  has_rich_text :content
 
   validates :title, presence: true, length: { in: 2..30 }
   validates :content, presence: true, length: { minimum: 3 }
+
+  def image_as_thumbnail
+    image.variant(resize_to_limit: [300, 300])
+  end
 end
