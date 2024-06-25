65268c5fd7eb946e366c69e4aa0702027e3c78da
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-25 12:05:20 -0700

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
