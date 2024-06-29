fe00706f43f45d153de34f5f25a949f9fda0f209
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-29 09:23:41 -0700

    Add css to requests on profile page and prevent non-accepted requests from showing up as following/follower

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
     @requests = @user.follower_relationships.where(accepted:false)
+    @followers = @user.follower_relationships.where(accepted:true)
+    @followings = @user.following_relationships.where(accepted:true)
   end
 end
