daa2d3aa7268668c44fbcf52ecaad03e8140339b
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-29 08:43:55 -0700

    Update css for accept/decline actioncable requests

--- app/javascript/channels/follow_channel.js
+++ app/javascript/channels/follow_channel.js
@@ -21,17 +21,17 @@ const followChannel = consumer.subscriptions.create("FollowChannel", {
 
   request(data) {
     return `<article id="followrequest">
-                <p>${data.user_id.email} would like to follow you. Accept follow request?</p>
-                  <form action="/follows/${data.user_id.id}" method="patch" id="accept-form">
-                    <button type="submit" name="follow[follower]" onclick="document.getElementById('followrequest').hidden=true" value=${data.user_id.id}>Accept</button>
-                    <button type="decline" name="follow[follower]" onclick="history.go(0)" value=${data.user_id.id} formmethod="delete" formaction="/follows/${data.user_id.id}">Decline</button>
+                <p class="m-0 text-center">${data.user_id.user_name} would like to follow you. Accept follow request?</p>
+                  <form action="/follows/${data.user_id.id}" method="patch" id="accept-form" class="text-center">
+                    <button type="submit" name="follow[follower]" onclick="document.getElementById('followrequest').hidden=true" value=${data.user_id.id} class="bg-orange-400 hover:bg-orange-300 text-white font-bold text-xs py-1 px-4 rounded">Accept</button>
+                    <button type="decline" name="follow[follower]" onclick="history.go(0)" value=${data.user_id.id} formmethod="delete" formaction="/follows/${data.user_id.id}" class="bg-orange-400 hover:bg-orange-300 text-white font-bold text-xs py-1 px-4 rounded">Decline</button>
                   </form>
             </article>`
   },
 
   accept(data) {
-    return `<article class="accept-request">
-                <p>${data.user_id.email} has accepted your request!</p>
+    return `<article class="accept-request text-center">
+                <p class="m-0 p-4">${data.user_id.user_name} has accepted your request!</p>
             </article>`
   },
 
